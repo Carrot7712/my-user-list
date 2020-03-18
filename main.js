33c04de2f1e00d368c5c0901cae5f25035ac5205
Carrot7712 <carrot7712@gmail.com>
2020-03-18 14:27:41 +0800

    modal finished

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 ;(function() {
   //取資料
   const BASE_URL = 'https://lighthouse-user-api.herokuapp.com/'
-  const INDEX_URL = BASE_URL + 'api/v1/users'
+  const INDEX_URL = BASE_URL + 'api/v1/users/'
   //存放API進來的資料
   const data = []
-
   //找元件
   const dataPanel = document.getElementById('data-panel')
   
   // ===============EventListeners======================
-  //listen to data panel
-  document.addEventListener('DOMContentLoaded',function() {
     //透過API取得使用者資料
     axios
     .get(INDEX_URL)
     .then(response => {
       console.log(response.data.results)
       data.push(...response.data.results)
-      displayData(data)
+      displayCard(data)
     })
     .catch(error => console.log(error))
+
+  //listen to data panel
+  dataPanel.addEventListener('click', function () { 
+    if (event.target.matches('.btn-show-user')) { 
+      console.log(event.target.dataset.id)
+      displayModal(event.target.dataset.id)
+    }
   })
+  //===============functions======================
 //show cards
-function displayData(data){
+  function displayCard(data) {
     let htmlContent = ''
     data.forEach(item => {
-        htmlContent += `
+      htmlContent += `
         <div class="col-sm-3">
             <div class="card mb-2">
                 <img class="card-img-top " src="${item.avatar}" alt="Card image cap">
@@ -48,23 +53,56 @@ function displayData(data){
             </div>
         </div>
         `
-        dataPanel.innerHTML=htmlContent
+      dataPanel.innerHTML = htmlContent
     })
-    
+  }
+    //show modal
+  function displayModal(id) { 
+    //get elements
+    const userAvatar=document.getElementById('user-avatar')
+    const userName = document.getElementById('user-name')
+    const userBd = document.getElementById('user-bd')
+    const userRegion =document.getElementById('user-region')
+    const userEmail = document.getElementById('user-email')
+    const userCreated = document.getElementById('user-created')
+    const userUpdated = document.getElementById('user-updated')
+    //set request url
+    const url = INDEX_URL + id
+    console.log(url)
+    //send request to Show API
+    axios
+      .get(url)
+      .then(response => { 
+        const data = response.data
+        console.log(response.data.avatar)
+        //insert data into modal UI
+        userAvatar.innerHTML = `
+        <img src="${data.avatar}" alt="..." class="img-fluid">
+        `
+        userName.textContent = `${data.name} ${data.surname}`
+        userBd.innerHTML=`${data.birthday} age(${data.age})`
+        userRegion.innerHTML = `${data.region}`
+        userCreated.innerHTML = `created at ${data.created_at}`
+        userUpdated.innerHTML=`updated at ${data.updated_at}`
+      })
+      .catch(error => console.log(error))
+    //insert data into modal UI
+  }
+
   let generateModal=`
     <div class="modal-img">
     <img id="user-avatar" src="" alt="...">
   </div>
   <div class="modal-detail">
-    <ul id="user-information">
-      <li>Name</li>
-      <li>birthday(age)</li>
-      <li>region</li>
-      <li>email</li>
-      <li>created at</li>
-      <li>updated at</li>
+    <ul id="show-user-information">
+      <li id="user-name">Name</li>
+      <li id="user-bd">birthday(age)</li>
+      <li id="user-region">region</li>
+      <li id="user-email">email</li>
+      <li id="user-created">created at</li>
+      <li id="user-updated">updated at</li>
     </ul>
   </div>
   `
-}
+
 })()
